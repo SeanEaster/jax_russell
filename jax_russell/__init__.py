332c32f055bb29cf72a463b62c470693f73027e0
Sean Easter <sean@seaneaster.com>
2023-09-08 08:18:29 -0500

    tested support for CRR and RB trees (#1)
    Co-authored-by: SeanEaster <sean@easter.ai>

--- jax_russell/__init__.py
+++ jax_russell/__init__.py
@@ -4,4 +4,12 @@ __author__ = """Sean Easter"""
 __email__ = 'sean@easter.ai'
 __version__ = '0.1.0'
 
-# import crr
+from jax_russell.trees import CRRBinomialTree, ExerciseValuer, MaxValuer, SoftplusValuer, ValuationModel
+
+__all__ = [
+    "CRRBinomialTree",
+    "ExerciseValuer",
+    "MaxValuer",
+    "SoftplusValuer",
+    "ValuationModel",
+]
