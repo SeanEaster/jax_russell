fbc7a28b9a180370ef414ab8065008280c29bcc9
SeanEaster <sean@easter.ai>
2023-09-29 08:16:03 -0500

    Bump version: 0.1.1 → 0.2.0

--- jax_russell/__init__.py
+++ jax_russell/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Sean Easter"""
 __email__ = 'sean@easter.ai'
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 
 
 from jax_russell.base import StockOptionContinuousDividendMixin, StockOptionMixin, ValuationModel
