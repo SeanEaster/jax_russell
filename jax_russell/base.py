8fabc7b592da3c74abdbb7273fbe3c0c3442d063
SeanEaster <sean@easter.ai>
2023-09-29 17:04:29 -0500

    GH actions updates, revert tolerance

--- jax_russell/base.py
+++ jax_russell/base.py
@@ -134,7 +134,7 @@ class ValuationModel(abc.ABC):
             residuals = expected - self(*bound_arguments.args, **bound_arguments.kwargs)
             return jnp.mean(residuals**2)
 
-        solver = jaxopt.BFGS(objective, tol=1e-5)
+        solver = jaxopt.BFGS(objective)
         res = solver.run(
             init_params,
             expected=expected_option_values,
